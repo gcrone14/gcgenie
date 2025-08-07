cf979861e6c2b0f30bf936349710d86b40ab5ea6
Gabriel Crone <gcrone14@gmail.com>
2025-08-07 11:22:37 -0400

    Added new count_plot() function

--- R/count_functions.R
+++ R/count_functions.R
@@ -26,6 +26,8 @@
 #' dat |> freq_count(native_language)
 #' dat |> freq_count(occupation, native_language)
 freq_count <- function(dat, ..., sort = TRUE) {
+    if(!valid_data(dat)) stop("Data is not a matrix, data frame, or tibble.")
+
     dat |>
         dplyr::count(..., sort = sort) |>
         dplyr::mutate(perc_n = n/sum(n)*100)
@@ -49,26 +51,42 @@ freq_count <- function(dat, ..., sort = TRUE) {
 #' unique_count(df, names)
 #'
 unique_count <- function(dat, var, delim = ",", ...) {
+    if(!valid_data(dat)) stop("Data is not a matrix, data frame, or tibble.")
     dat |>
         tidyr::separate_longer_delim({{var}}, delim) |>
         freq_count({{var}}, ...)
 }
 
-# count_plot <- function(dat, var, n = 30, label = FALSE, x_nudge = NULL, ...) {
-#     var <- rlang::enquo(var)
-#
-#     # Define plot
-#     p <- dat %>%
-#         mutate(!!var := fct_reorder(!!var, n)) %>%
-#         filter(!is.na(!!var), !!var != "") %>%
-#         head(n = n) %>%
-#         ggplot(aes(x = !!var, y = n)) +
-#         geom_col(color = "black", fill = "royalblue") +
-#         coord_flip() +
-#         labs(y = "Number of retractions", ...)
-#
-#     # If label = TRUE, add labels
-#     if(label == TRUE) p + geom_text(aes(label = n), nudge_y = x_nudge)
-#     else p
-# }
-#
+#' Generate a Count Plot from a Count Table
+#'
+#' @param dat Frequency table (tibble, data.frame, or matrix) that includes a categorical (counted or grouped) and a corresponding count column (usually, 'n').
+#' @param var The categorical variable in the frequency table, unquoted.
+#' @param count_var The count of the variable, usually 'n', unqouted. By default, it is assumed to be 'n'.
+#' @param head_n The number of final unique values to include in the plot.
+#' @param label (Optional) Should the plot
+#'
+#' @returns A ggplot2 column plot of frequencies.
+#' @export
+#'
+#' @examples
+#' # TBD
+count_plot <- function(dat, var = NULL, count_var = n, head_n = nrow(dat), label = FALSE) {
+    if(!valid_data(dat)) {
+        stop("Data is not a matrix, data frame, or tibble.")
+    }
+
+    if (is.null(var)) {
+        stop("var is missing. Please specify the variable to create the count plot for.")
+    }
+    p <- dat %>%
+        dplyr::mutate({{ var }} := forcats::fct_reorder({{ var }}, {{ count_var }})) %>%
+        dplyr::filter(!is.na({{ var }}), {{ var }} != "") |>
+        head(head_n) |>
+        ggplot2::ggplot(ggplot2::aes(x = {{ var }}, y = {{ count_var }})) +
+        ggplot2::geom_col(color = "black", fill = "royalblue") +
+        ggplot2::coord_flip()
+    if(label) p + ggplot2::geom_text(ggplot2::aes(label = {{ count_var }}), hjust = -0.1)
+    else p
+}
+
+
