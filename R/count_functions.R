bf7c2f77dd57c8f185237cac76629542547f6318
Gabriel Crone <Gabe@Danielles-Air.ht.home>
2025-08-07 12:52:21 -0400

    Updated examples + fixed enquo error

--- R/count_functions.R
+++ R/count_functions.R
@@ -13,6 +13,8 @@
 #' Defaults to TRUE.
 #'
 #' @import dplyr
+#' @import rlang
+#'
 #' @returns Returns an object of the same class as dat: a tibble, data frame, or matrix.
 #' @export
 #'
@@ -44,11 +46,9 @@ freq_count <- function(dat, ..., sort = TRUE) {
 #' @export
 #'
 #' @examples
-#' # Sample data frame
-#' df <- tibble::tibble(names = c("Jane,Joe", "Joe", "Joe,Kai", "Kai"))
+#' df <- tibble::tibble(person = c("Jane,Joe", "Joe", "Joe,Kai", "Kai"))
 #'
-#' # Count unique city mentions across delimited entries
-#' unique_count(df, names)
+#' df |> unique_count(person)
 #'
 unique_count <- function(dat, var, delim = ",", ...) {
     if(!valid_data(dat)) stop("Data is not a matrix, data frame, or tibble.")
@@ -59,33 +59,58 @@ unique_count <- function(dat, var, delim = ",", ...) {
 
 #' Generate a Count Plot from a Count Table
 #'
+#' @description
+#' Generates a column plot from a frequency table,
+#' with categories sorted by descending frequency.
+#'
 #' @param dat Frequency table (tibble, data.frame, or matrix) that includes a categorical (counted or grouped) and a corresponding count column (usually, 'n').
 #' @param var The categorical variable in the frequency table, unquoted.
 #' @param count_var The count of the variable, usually 'n', unqouted. By default, it is assumed to be 'n'.
 #' @param head_n The number of final unique values to include in the plot.
-#' @param label (Optional) Should the plot
+#' @param label (Optional) If TRUE, will provide text of the frequnecy count to the right of each bar.
 #'
 #' @returns A ggplot2 column plot of frequencies.
 #' @export
 #'
 #' @examples
-#' # TBD
+#' df <- tibble::tibble(person = c("Jane,Joe", "Joe", "Joe,Kai", "Kai"))
+#'
+#' df |> unique_count(person) |> count_plot(person)
+#' # With labels
+#' df |> unique_count(person) |> count_plot(person, label = TRUE)
+#'
 count_plot <- function(dat, var = NULL, count_var = n, head_n = nrow(dat), label = FALSE) {
+    # Convert "var" and "count_var" to symbols
+    var <- rlang::enquo(var)
+    count_var <- rlang::enquo(count_var)
+
     if(!valid_data(dat)) {
         stop("Data is not a matrix, data frame, or tibble.")
     }
 
-    if (is.null(var)) {
+    if (rlang::quo_is_null(var)) {
         stop("var is missing. Please specify the variable to create the count plot for.")
     }
-    p <- dat %>%
-        dplyr::mutate({{ var }} := forcats::fct_reorder({{ var }}, {{ count_var }})) %>%
-        dplyr::filter(!is.na({{ var }}), {{ var }} != "") |>
-        head(head_n) |>
-        ggplot2::ggplot(ggplot2::aes(x = {{ var }}, y = {{ count_var }})) +
-        ggplot2::geom_col(color = "black", fill = "royalblue") +
-        ggplot2::coord_flip()
-    if(label) p + ggplot2::geom_text(ggplot2::aes(label = {{ count_var }}), hjust = -0.1)
+
+    p <- dat |>
+    dplyr::mutate(
+        !!var := forcats::fct_reorder(!!var, !!count_var)
+    ) |>
+    dplyr::filter(!is.na(!!var), !!var != "") |>
+    head(head_n) |>
+    ggplot2::ggplot(ggplot2::aes(x = !!var, y = !!count_var)) +
+    ggplot2::geom_col(color = "black", fill = "royalblue") +
+    ggplot2::coord_flip()
+
+    if(label) {
+        # max_val <- max(dplyr::pull(dat, !!count_var), na.rm = TRUE)
+        # p +
+        #     ggplot2::geom_text(
+        #         mapping = ggplot2::aes(label = !!count_var, x = !!var, y = !!count_var),
+        #         position = ggplot2::position_identity(),
+        #         hjust = ifelse(dplyr::pull(dat, !!count_var) > 0.01 * max_val, -0.1, 1.1))
+    }
+    # if(label) p + ggplot2::geom_text(ggplot2::aes(label = {{ count_var }}), hjust = -0.1)
     else p
 }
 
