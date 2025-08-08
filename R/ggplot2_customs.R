2a422c2e1f5ac2fae987424fdd68f0886f984994
Gabriel Crone <Gabe@Danielles-MacBook-Air.local>
2025-08-08 10:10:47 -0400

    Added custom histogram functions

---
+++ R/ggplot2_customs.R
@@ -0,0 +1,37 @@
+#' Custom ggplot2 histogram
+#'
+#' @description
+#' ggplot2::goem_histogram() with useful defaults (e.g., black color,
+#' royal blue fill, and useful boundary conditions.)
+#'
+#' @param color (Optional) Color of the outline of each bar.
+#' @param fill (Optional) Color of the bars themselves.
+#' @param ... Arguments passed to ggplot2::geom_histogram().
+#'
+#' @returns A histogram plot.
+#' @export
+#'
+#' @examples
+#' sample_dat <- data.frame(x = rnorm(10000, mean = 10, 1))
+#' # Regular plot
+#' sample_dat |>
+#'     ggplot2::ggplot(ggplot2::aes(x)) +
+#'     geom_histogram_custom(binwidth = 0.5)
+#'
+#' # Alter color and fill
+#' sample_dat |>
+#'     ggplot2::ggplot(ggplot2::aes(x)) +
+#'     geom_histogram_custom(binwidth = 0.5, color = "grey", fill = "gold")
+#'
+geom_histogram_custom <- function(..., color = "black", fill = "royalblue") {
+  ggplot2::geom_histogram(
+    color = color,
+    fill = fill,
+    boundary = 1,
+    ...
+  )
+}
+
+theme_hist <- function() {
+
+}
